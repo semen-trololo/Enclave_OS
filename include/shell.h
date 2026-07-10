5f7d85ec5ba7f6f07883f1b132c87da216e91a82
Semen <95562395+semen-trololo@users.noreply.github.com>
2026-07-10 14:50:46 +0300

    Update shell.h

--- include/shell.h
+++ include/shell.h
@@ -4,4 +4,10 @@
 // Запуск бесконечного цикла командной оболочки
 void shell_run(void);
 
+// [ДЕНЬ 10] Test Runner Entry Point
+void test_init(void);
+
+// [ДЕНЬ 10] Stress Test Handler
+void handle_stress(int argc, char args[4][64]);
+
 #endif
